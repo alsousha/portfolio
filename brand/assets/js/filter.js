304d0b939b10ff298277ebe52e49f64ee0a6e806
Alsu Bogdanova <47866010+alsousha@users.noreply.github.com>
2021-07-25 10:47:57 +0300

    Add files via upload

---
+++ brand/assets/js/filter.js
@@ -0,0 +1,14 @@
+$( function() {
+    $( "#slider-range" ).slider({
+      range: true,
+      min: 0,
+      max: 500,
+      values: [ 52, 400 ],
+      slide: function( event, ui ) {
+        $( "#amount1" ).val( "$" + ui.values[ 0 ]);
+        $( "#amount2" ).val( "$" + ui.values[ 1 ]);
+      }
+    });
+    $( "#amount1" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ));
+    $( "#amount2" ).val( "$" + $( "#slider-range" ).slider( "values", 1 ) );
+  } );
---
+++ brand/assets/js/js.js
@@ -0,0 +1,50 @@
+(function(){
+    const triangle = document.querySelector(".form__browse");
+    const dropForm = document.querySelector(".drop__form-position");
+    
+    triangle.addEventListener("click", function(event){
+        event.preventDefault;
+        dropForm.classList.add("drop__active");
+    })
+    dropForm.addEventListener("mouseleave", function(){
+        dropForm.classList.remove("drop__active");
+    })
+
+})();
+$( function() {
+    $( "#slider-range" ).slider({
+      range: true,
+      min: 0,
+      max: 500,
+      values: [ 75, 300 ],
+      slide: function( event, ui ) {
+        $( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
+      }
+    });
+    $( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) +
+      " - $" + $( "#slider-range" ).slider( "values", 1 ) );
+    
+    
+    $('.product__content-title').on('click', function(){
+      $(this).toggleClass('triangle-close');
+      $(this).toggleClass('triangle-open')
+    $(this).next().slideToggle();
+    });
+
+    $('.single__product-wrap').slick({
+      prevArrow: '<button class="banner-section__slider-btn banner-section__slider-btnprev btn"><img src="assets/img/arrow-left.svg" alt=""></button>',
+      nextArrow: '<button class="banner-section__slider-btn banner-section__slider-btnnext btn"><img src="assets/img/arrow-right.svg" alt=""></button>',
+      responsive: [
+          {
+            breakpoint: 969,
+            settings: {
+              arrows: false
+            }
+          },
+      ]
+    });
+
+
+} );
+
+  
